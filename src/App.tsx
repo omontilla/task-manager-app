4eb7899567589fc865855ff7f1c2c24c51305d7a
Oscar Montilla <omontilla@unow.es>
2024-12-30 14:35:54 -0400

    dejando como estaba

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App: React.FC = () => {
                         notificationProvider={useNotificationProvider}
                         resources={[
                             {
-                                name: "task",
+                                name: "api/tasks",
                                 list: "/task",
                                 create: "/task/create",
                                 edit: "/task/:id",
