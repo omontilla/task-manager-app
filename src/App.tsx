5d9212e458e3bf754191a8828e85f361da58cec4
Oscar Montilla <omontilla@unow.es>
2024-12-30 15:18:56 -0400

    probando env

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ import {TaskList, TaskCreate, TaskEdit, TaskShow} from "./pages";
 import {I18nextProvider} from "react-i18next";
 import i18n from "i18next";
 
-const API_URL = "https://task-manager-core-production.up.railway.app";
-//const API_URL = "http://localhost:3000"
+console.log(process.env.REACT_APP_API_URL);
+const API_URL = process.env.REACT_APP_API_URL
 
 const App: React.FC = () => {
     return (
